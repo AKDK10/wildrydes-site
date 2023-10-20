95b243c2c4cda6312ccd7ccb73aa088a32d5d23c
Akla Djankla <djsolangea@gmail.com>
2023-10-20 14:39:21 +0100

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2 (eu-west-2 is London)
+        userPoolId: 'eu-west-2_3qFDx6MqI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '54pavmikk7ctjtee0d6elsh3pc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2 (eu-west-2 is London)
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
